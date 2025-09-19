bc11552de8b5826c5b40a9738b5250cbf92b9249
Michael Savage <savagemichael1707@gmail.com>
2025-09-19 08:28:50 +0100

    Hi

--- crosswords/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Crossword(models.Model):
-    code = models.CharField(max_length=10000000000)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from thepelicancrossing!")
-
-
-if __name__ == "__main__":
-    main()
